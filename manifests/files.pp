51da1efe63c60c27aebad6b24e41d0753aeeee1f
Balazs Nagy <julsevern@gmail.com>
2013-05-29 19:29:10 +0200

    av and spam filtering for amavis

--- manifests/files.pp
+++ manifests/files.pp
@@ -3,7 +3,9 @@ class amavisd::files(
   $dbname = undef,
   $dbpass = undef,
   $domains_table = 'domains',
-  $domains_domain = 'domain'
+  $domains_domain = 'domain',
+  $virus_checking = false,
+  $spam_checking = false
 ) inherits amavisd::params {
   File {
     owner  => 'root',
@@ -17,6 +19,20 @@ class amavisd::files(
     undef   => 'absent',
     default => 'present'
   }
+  $avensure = $virus_checking ? {
+    true    => 'present',
+    default => 'absent'
+  }
+  $avspamensure = $virus_checking ? {
+    true  => 'present',
+    false => $spam_checking ? {
+      true  => 'present',
+      false => 'absent'
+    }
+  }
+
+  $conf = $amavisd::params::conf
+  $confd = "${amavisd::params::conf}/conf.d"
 
   file {
     $conf:
@@ -31,13 +47,21 @@ class amavisd::files(
       recurse => true,
       source  => 'puppet:///modules/amavisd/en_US'
       ;
-    "${conf}/conf.d":
+    $confd:
       ensure  => $directory,
       mode    => '0755',
       recurse => true,
       source  => 'puppet:///modules/amavisd/conf.d'
       ;
-    "${conf}/conf.d/50-sql":
+    "${confd}/15-av_scanners":
+      ensure  => $avensure,
+      content => template('amavisd/15-av_scanners.erb')
+      ;
+    "${confd}/15-content_filter_mode":
+      ensure  => $avspamensure,
+      content => template('amavisd/15-content_filter_mode.erb')
+      ;
+    "${confd}/50-sql":
       ensure  => $sqlensure,
       mode    => '0600',
       content => template('amavisd/50-sql.erb')
--- spec/classes/files_spec.rb
+++ spec/classes/files_spec.rb
@@ -16,6 +16,8 @@ describe 'amavisd::files' do
       should contain_file('/etc/amavis/en_US').with(dirstats)
       should contain_file('/etc/amavis/conf.d').with(dirstats)
       should contain_file('/etc/amavis/conf.d/50-sql').with_ensure('absent')
+      should contain_file('/etc/amavis/conf.d/15-av_scanners').with_ensure('absent')
+      should contain_file('/etc/amavis/conf.d/15-content_filter_mode').with_ensure('absent')
     end
   end
 
@@ -35,6 +37,34 @@ $defang_virus  = 1;  # MIME-wrap passed infected mail
 $defang_banned = 1;  # MIME-wrap passed mail containing banned name
 1;
         EOF
+    end
+  end
+
+  context "(with av checking enabled)" do
+    let(:params) {{virus_checking: true}}
+    it do
+      should contain_file('/etc/amavis/conf.d/15-av_scanners').with(
+        ensure: 'present',
+        owner: 'root',
+        group: 'root',
+        mode: '0644')
+      should contain_file('/etc/amavis/conf.d/15-content_filter_mode').with(
+        ensure: 'present',
+        owner: 'root',
+        group: 'root',
+        mode: '0644')
+    end
+  end
+
+  context "(with spam checking enabled)" do
+    let(:params) {{spam_checking: true}}
+    it do
+      should contain_file('/etc/amavis/conf.d/15-av_scanners').with_ensure('absent')
+      should contain_file('/etc/amavis/conf.d/15-content_filter_mode').with(
+        ensure: 'present',
+        owner: 'root',
+        group: 'root',
+        mode: '0644')
     end
   end
 end
